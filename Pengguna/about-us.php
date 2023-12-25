300f17e2df57b4d62298f2be74274ae94a424a7a
Dhikaaprtmaa <144656642+Dhikaaprtmaa@users.noreply.github.com>
2023-12-25 15:08:46 +0700

    Add files via upload

---
+++ Pengguna/about-us.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Toko Online | About Us</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <?php require "navbar.php" ?>    
+
+    <!-- banner -->
+    <div class="container-fluid banner-produk d-flex align-items-center">
+        <div class="container">
+            <h1 class="text-white text-center">About Us</h1>
+        </div>
+    </div>
+
+    <!-- main -->
+    <div class="container-fluid py-5">
+        <div class="container fs-5 text-center">
+            <p>
+                Selamat datang di UrbanChic, rumah bagi pencinta gaya perkotaan yang membumi dan penuh gaya. Sejak berdiri, UrbanChic telah menjadi penjelajah tak kenal lelah di dunia fashion, menggabungkan kegelapan dan cahaya kota dalam setiap jahitan kami. Di hati UrbanChic, kami tidak hanya menjual pakaian, tetapi juga menceritakan kisah gaya hidup perkotaan yang penuh semangat dan keberanian.
+            </p>
+            <p>
+                UrbanChic lahir dari visi untuk menciptakan ruang di mana kreativitas dan individualitas berkumpul. Kami percaya bahwa gaya tidak boleh terbatas oleh aturan atau tren, melainkan harus menjadi refleksi dari siapa Anda sebenarnya. Koleksi kami yang eksklusif dipilih dengan teliti, mencakup pakaian dari desainer terkemuka yang merangkul semangat inovasi dan gaya pribadi.
+
+                Kami bangga menjadi penghubung antara dunia fashion perkotaan yang dinamis dan individu yang berani mengekspresikan diri mereka melalui gaya. Setiap produk UrbanChic adalah pernyataan fashion yang berbicara tentang keanggunan yang tulus dan gaya yang tak tergoyahkan. Dari sentuhan kasual yang modern hingga pernak-pernik yang menarik, kami menawarkan pilihan pakaian yang memenuhi kebutuhan setiap perjalanan Anda.
+            </p>
+            <p>
+                Kami memahami bahwa kecantikan sejati tidak hanya ada di permukaan, melainkan terpancar dari rasa percaya diri dan kenyamanan diri. Oleh karena itu, UrbanChic berkomitmen untuk memberikan pengalaman belanja yang istimewa. Tim kami selalu berada di sini untuk memberikan panduan dan inspirasi, menjadikan setiap kunjungan ke UrbanChic bukan hanya sekadar pembelian, tetapi pengalaman fashion yang membangun kenangan.
+
+                Sebagai bagian dari komunitas UrbanChic, Anda tidak hanya memilih pakaian; Anda bergabung dengan gerakan kecantikan yang merayakan keunikan dan menyambut berbagai jenis kepribadian. Selamat datang di UrbanChic, di mana setiap penemuan fashion adalah perjalanan menuju kesempurnaan dalam keanggunan urban. Bersama-sama, kita merayakan kehidupan, gaya, dan semangat tak terbatas di balik setiap pakaian yang dipilih dengan cermat.
+            </p>
+        </div>
+    </div>
+
+    <!-- footer --> 
+    <?php require "footer.php"; ?>
+
+    <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="fontawesome/js/all.min.js"></script>
+
+</body>
+</html>
---
+++ Pengguna/footer.php
@@ -0,0 +1,26 @@
+<div class="container-fluid py-5 content-subscribe text-light">
+    <div class="container">
+        <h5 class="text-center mb-4">Contact Us</h5>
+        <div class="row justify-content-center">
+            <div class="col-sm-1 d-flex justify-content-center mb-2">
+                <a href="https://instagram.com/dhikaaprtma_?igshid=MzMyNGUyNmU2YQ%3D%3D&utm_source=qr"><i class="fab fa-instagram fs-2"></i></a>
+            </div>
+            <div class="col-sm-1 d-flex justify-content-center mb-2">
+                <a href="http://facebook.com"><i class="fab fa-facebook fs-2"></i></a>
+            </div>
+            <div class="col-sm-1 d-flex justify-content-center mb-2">
+                <a href="http://twitter.com"><i class="fab fa-twitter fs-2"></i></a>
+            </div>
+            <div class="col-sm-1 d-flex justify-content-center mb-2">
+                <a href="https://wa.me/62081312840727"><i class="fab fa-whatsapp fs-2"></i></a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container-fluid py-3 bg-dark text-light">
+    <div class="container d-flex justify-content-between">
+        <label><i class="fa-regular fa-copyright fs-4"></i> 2023 UrbanChic</label>
+        <label>Created by Andhika Pratama K</label>
+    </div>
+</div>
